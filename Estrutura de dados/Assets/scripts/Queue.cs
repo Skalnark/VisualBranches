28264e98398f8ae734b21f92a7bb20ad75d6d6a2
Swehtam <mathewsalves1@outlook.com>
2017-10-19 12:33:29 -0300

    Add files via upload

--- Estrutura de dados/Assets/scripts/Queue.cs
+++ Estrutura de dados/Assets/scripts/Queue.cs
@@ -1,62 +1,84 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
-public class Queue : MonoBehaviour {
+public class Queue {
 	private Node begin;
 	private Node end;
-	private int nElements = 0;
-
-	public bool empty(){
-		if(nElements == 0)
-			return true;
-		else return false;
+	private int nElements;
+	
+	public Queue() {
+		nElements = 0;
 	}
 
-	public int size(){
+	public bool empty () {
+		if (nElements == 0)
+			return true;
+		else
+			return false;
+	}
+
+	public int size () {
 		return nElements;
 	}
 
-	public int first(){
-		if(empty()){
+	public int first () {
+		if (empty())
 			return -1;
-		}
-		return begin.content;
+
+		return begin.getContent();
 	}
 
-	public void push(int value){
+	public void push (int value) {
+		GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
 		Node newNode = new Node();
-		newNode.content = value;
+		newNode.setSquare(cube);
+		newNode.setContent(value);
 
-		if(empty()){
+	   if (empty()){   
 			begin = newNode;
-		}
-		else{
+			newNode.getSquare().transform.position = new Vector3(-6.3f, 0,0);
+	   }
+	   else {
 			end.setNext(newNode);
+			int pos = tamanho();
+			newNode.getSquare().transform.position = new Vector3(-6.3f +(float) pos, 0, 0); 
 		}
-
 		end = newNode;
-
 		nElements++;
 	}
 
-	public void pull(){
-		if(empty()){
-			return;
-		}
-		int value = first();
-		Node p = begin;
+	public void pull() {
+		if (empty()) {
+			//printar q n tem nada na fila	        
+			return; 
+	    }
 
-		if(begin == end){
+		//int value = first();
+		Node p = begin;
+		if (begin == end){
+			Destroy(begin.getSquare());
 			end = null;
 			begin = null;
-		}
-		else{
+	 	}
+	 	else{
+			Destroy(begin.getSquare());	
 			begin = p.getNext();
-		}
+			Node aux = begin;
+			int number = 0;
 
-		p = null;
-		nElements--;
+			while(number < 5){
+				Vector3 position = this.transform.position;
+				position.x--;
+				aux.getSquare().transform.position = position;
+				aux = aux.getNext();
+				number++;
+			}
+
+	 	}	
+
+	    p= null;
+
+	    nElements--;
 	}
 
+	
+	
 }
