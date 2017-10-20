b8e122dd5e0d359257686028641a8531264a22d5
Skalnark <skalnark@live.com>
2017-10-20 00:43:25 -0300

    melhorias no script de cenas

---
+++ Estrutura de dados/Assets/scripts/MainMenuNavigator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuNavigator : MonoBehaviour {
+
+    //Esse script sai da MainScene para a ListScene
+    public void GoToList()
+    {
+        SceneManager.LoadScene("ListScene");//carrega a cena da lista
+    }
+
+    public void GoToQueue()
+    {
+        SceneManager.LoadScene("QueueScene");//carrega a cena da fila
+    }
+
+    public void GoToStack()
+    {
+        SceneManager.LoadScene("StackScene");//carrega a cena da pilha
+    }
+
+    public void GoToMain()
+    {
+        SceneManager.LoadSceneAsync(0); //vai para a main scene
+    }
+    
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ Estrutura de dados/Assets/scripts/QueuePainel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class QueuePainel : MonoBehaviour {
+
+    //abre o painel com os numeros a serem adicionados a fila
+    
+    public void OpenPanel()
+    {
+            if(!SceneManager.GetSceneByName("numericPanel").isLoaded)
+                SceneManager.LoadSceneAsync("numericPanel", LoadSceneMode.Additive);
+    }
+
+    public void ClosePanel()
+    {
+        SceneManager.UnloadSceneAsync("numericPanel");
+    }
+}
